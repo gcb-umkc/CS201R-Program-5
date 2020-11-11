8f5ee0cd524790de5018511c028d8171c5fe3f2a
gcb-umkc <gcbz9k@mail.umkc.edu>
2020-11-11 16:12:04 -0600

    Added initial code and .gitignore

--- Program-5.cpp
+++ Program-5.cpp
@@ -1,20 +1,39 @@
-// Program-5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+using namespace std;
 
+static int goal = 35;
+int count = 0;
+
+void GameFunction(int numTokens, int turnsLeft, int count) {
+
+    if (turnsLeft == 0) {
+        cout << "Unable to reach number of tokens" << endl;
+        return;
+    }
+    else if (numTokens == goal) {   
+        cout << "Reached " << goal << " tokens at " << count << endl;
+        return;
+    }
+    else {
+        if (numTokens < goal) {
+            numTokens += 25;
+        }
+        else {
+            if (numTokens % 2) {
+                numTokens /= 2;
+            }
+            else {
+                numTokens += 25;
+            }
+        }
+        cout << "Tokens Remaining: " << numTokens << endl;
+        count++;
+        GameFunction(numTokens, turnsLeft - 1, count);
+    }
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    GameFunction(13, 100, 0);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
